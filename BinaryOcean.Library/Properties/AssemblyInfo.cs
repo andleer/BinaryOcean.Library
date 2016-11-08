dc58224c2bd45c2b96bd04e98bd2ac68e2a28dd8
Andrew Robinson <andrew@binaryocean.com>
2016-11-08 10:51:38 -0800

    Update AssemblyInfo.cs

--- BinaryOcean.Library/Properties/AssemblyInfo.cs
+++ BinaryOcean.Library/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Extentions")]
+[assembly: AssemblyTitle("BinaryOcean.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Extentions")]
+[assembly: AssemblyProduct("BinaryOcean.Library")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
